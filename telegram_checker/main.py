d7ebe881cc08dbfdec3a739c5a549c23cae5811c
aleksandr <mac@MacBook-Air-Mas.local>
2021-02-09 22:58:44 +0300

    add functionality

--- telegram_checker/main.py
+++ telegram_checker/main.py
@@ -1,23 +1,88 @@
 import argparse
 import logging
 import socks
+import json
 import asyncio
 
 from telethon import TelegramClient, events, errors, sync, functions, types
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.functions.contacts import ImportContactsRequest, GetContactsRequest, DeleteContactsRequest
+from telethon.tl.types import InputPhoneContact
 from telethon.sessions import StringSession
+from telethon.errors.rpcerrorlist import FloodWaitError
 
-logging.basicConfig(format='[%(asctime)s] - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] - %(message)s', level=logging.INFO)
 
 API_ID = 0
 API_HASH = ""
 s_string = ""
 
-def check_phone(phone):
-    raise NotImplementedError
+def confirm_result(raw, data):
+    results_file = open("checked.json", "r")
+    old_data = results_file.read()
+    results_file.close()
+    results_file = open("checked.json", "w")
+    if old_data:
+        old_data = json.loads(old_data)
+        if old_data.get(raw, None):
+            for k in old_data[raw].keys():
+                if data[k] != old_data[raw][k]:
+                    old_data[raw][k] = data[k]
+        else:
+            old_data["raw"] = data
+        results_file.write(json.dumps(old_data))
+    else:
+        results_file.write(json.dumps({raw : data}))
+    results_file.close()
 
-def check_username(username):
-    raise NotImplementedError
+def configure_user(result, raw_data):
+    if result:
+        return  {
+                    "id" : raw_data.id,
+                    "first_name" : raw_data.first_name,
+                    "last_name" : raw_data.last_name,
+                    "username" : raw_data.username,
+                    "phone" : raw_data.phone,
+                    "is_bot" : raw_data.bot,
+                    "is_deleted" : raw_data.deleted,
+                    "has_photo" : True if raw_data.photo else False
+                }
+    else:
+        return {"error" : raw_data}
 
+async def check_phone(phone):
+    logging.info("Check phone {}".format(phone))
+    result = await client(ImportContactsRequest([InputPhoneContact(client_id=0, phone=phone, first_name="t", last_name="t")]))
+    found = False
+    for added_user in result.users:
+        found = True
+        logging.info("Got info about user with phone {}".format(phone))
+        user_info = await client(DeleteContactsRequest(id=[added_user.id])) # We delete user from contacts, because in "result" will be
+        confirm_result(raw=phone, data=configure_user(result=True, raw_data=user_info)) # not actual info in fields first_name and last_name
+                                                                                # because we set them to "t". We get actual info when we delete him
+    if not found:
+        confirm_result(raw=phone, data=configure_user(result=False, raw_data="End of requests limit or no user with this phone"))
+
+async def check_username(username):
+    logging.info("Check username {}".format(username))
+    while True:
+        try:
+            user_info = await client(GetFullUserRequest(username))
+            break
+        except ValueError:
+            logging.info("Value Error. No user with this username")
+            confirm_result(raw=username, data=configure_user(result=False, raw_data="User with this username not found"))
+            break
+        except TypeError:
+            logging.info("Type Error. This username is channel/group")
+            confirm_result(raw=username, data=configure_user(result=False, raw_data="Channel/group with this username exists. Not user"))
+            break
+        except FloodWaitError as err:
+            logging.info("Required sleep for {} seconds".format(err.seconds))
+            asyncio.sleep(err.seconds)
+            continue
+        logging.info("Got info about user with username {}".format(username))
+        confirm_result(raw=username, data=configure_user(result=True, raw_data=user_info))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Check username or phone number")
@@ -27,12 +92,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     proxy = (socks.SOCKS5, '127.0.0.1', 9050) if args.tor else None
 
-    client = TelegramClient(StringSession(s_string) API_ID, API_HASH, proxy=proxy).start()
+    client = TelegramClient(StringSession(s_string), API_ID, API_HASH, proxy=proxy).start()
 
     loop = asyncio.get_event_loop()
 
     if args.p:
-        phone = "".join(filter(str.isdigit, p))
+        phone = "".join(filter(str.isdigit, args.p))
         if len(phone) == 11 and phone.startswith("8"):
             phone = "7" + phone[1:]
         loop.run_until_complete(check_phone(phone))
